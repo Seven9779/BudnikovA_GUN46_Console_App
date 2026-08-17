bba02fa2ed3029078d2bf070ed80b898b4008ef9
Seven <97seven97@gmail.com>
2026-08-17 15:05:30 +0300

    calculator of bit operations

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        int result = 0;
+        if (int.TryParse(Console.ReadLine(), out int a) && int.TryParse(Console.ReadLine(), out int b))
+        {
+            Console.WriteLine("Valid operation & | ^");
+            switch (Console.ReadLine())
+            {
+                case "&":
+                    result = a & b;
+                    break;
+                case "|":
+                    result = a | b;
+                    break;
+                case "^":
+                    result = a ^ b;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input, please try again. Valid operation - &, |, ^");
+                    return;
+
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Invalid Input. Please enter a number");
+            return;
+        }
+
+        Console.WriteLine("\nCorrect\n");
+        Console.WriteLine($"decimal {result}");
+        Console.WriteLine($"binary {result:b8}");
+        Console.WriteLine($"hexadecimal {result:X}");
+
+
+    }
+}
+
+
