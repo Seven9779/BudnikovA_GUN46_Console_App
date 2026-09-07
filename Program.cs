8b57ed71d9350ec530bf21d40cd1784e5260a61b
Seven <97seven97@gmail.com>
2026-09-07 12:30:37 +0300

    work

--- Program.cs
+++ Program.cs
@@ -2,29 +2,33 @@
 {
     internal class Program
     {
-        public static bool InputOrExit(string input) { return input == "exit"; }
+        public static bool IsExit(string input) => input.Equals("exit", StringComparison.OrdinalIgnoreCase);
         private class ListString
         {
-            private readonly List<string> list = new List<string>() { "sss", "aaa" };
+            private readonly List<string> _names = new List<string>() { "sss", "aaa" };
             public void TaskLoop()
             {
-                Console.WriteLine("Task started. Enter exit to quit");
-                Console.WriteLine("Please input a line");
-                string line = Console.ReadLine();
+                while(true) 
+                {
+                    Console.WriteLine("Task started. Enter exit to quit");
+                    Console.WriteLine("Please input a line");
+                    string line = Console.ReadLine();
 
-                if (InputOrExit(line)) return;
+                    if (IsExit(line)) return;
 
-                list.Add(line);
-                Console.WriteLine(string.Join("\n", list));
-                Console.WriteLine("Enter another line to insert in the middle");
-                int middle = list.Count / 2;
-                line = Console.ReadLine();
+                    _names.Add(line);
+                    Console.WriteLine(string.Join("\n", _names));
+                    Console.WriteLine("Enter another line to insert in the middle");
+                    int middle = _names.Count / 2;
+                    line = Console.ReadLine();
 
-                if (InputOrExit(line)) return; 
+                    if (IsExit(line)) return;
 
-                list.Insert(middle, line);
-                Console.WriteLine("\n");
-                Console.WriteLine(string.Join("\n", list));
+                    _names.Insert(middle, line);
+                    Console.WriteLine();
+                    Console.WriteLine(string.Join("\n", _names));
+                }
+                
             }         
         }
 
@@ -32,98 +36,144 @@
         {
             public void TaskLoop()
             {
-                Console.WriteLine("Task started. Enter exit to quit");
                 Dictionary<string, int> students = new Dictionary<string, int>();
-                Console.WriteLine("Enter name student");
-                string name = Console.ReadLine();
-                if (InputOrExit(name)) return;
-                students.Add(name, 0);
 
-
-                Console.WriteLine("Enter grade student - from 2 to 5");
-                string grade = Console.ReadLine();
-                if (InputOrExit(grade)) return;
-
-                if (!int.TryParse(grade, out int a) || a < 2 || a > 5)
+                while (true)
                 {
-                    Console.WriteLine("Incorrect. grade student - from 2 to 5");
-                    return;
+                    Console.WriteLine("Task started. Enter exit to quit");
+                    Console.WriteLine("Enter name student");
+
+                    string name = Console.ReadLine();
+                    if (string.IsNullOrEmpty(name)) 
+                    { 
+                        Console.WriteLine("Incorrect Name Student"); 
+                        continue; 
+                    }
+
+                    if (IsExit(name)) return;
+                    Console.WriteLine("Enter grade student - from 2 to 5");
+
+                    string grade = Console.ReadLine();
+                    if (IsExit(grade)) return;
+                    if (!int.TryParse(grade, out int a) || a < 2 || a > 5)
+                    {
+                        Console.WriteLine("Incorrect. grade student - from 2 to 5");
+                        continue;
+                    }
+
+
+                    if (!students.TryAdd(name, a))
+                    {
+                        Console.WriteLine("This student already exists");
+                    }
+
+                    Console.WriteLine("Enter student name to find");
+
+                    string result = Console.ReadLine();
+                    if (string.IsNullOrEmpty(result))
+                    {
+                        Console.WriteLine("Incorrect Name Student");
+                        continue;
+                    }
+
+                    if (IsExit(result)) return;
+
+                    if (students.TryGetValue(result, out int b))
+                    {
+                        Console.WriteLine($"Name: {result}\nGrade: {b}");
+                    }
+
+                    else 
+                    { 
+                        Console.WriteLine("Incorrect input, this student doesn't exist");
+                        continue;
+                    }  
                 }
-
-                students[name] = a;
-
-                Console.WriteLine("Enter name student");
-
-                var result = Console.ReadLine();
-                if (InputOrExit(result)) return;
-                if (students.ContainsKey(result))
-                {
-                    Console.WriteLine($"Name: {result}\nGrade: {students[result]}");
-                }
-
-                else { Console.WriteLine("Incorrect input, this student doesn't exist"); } 
             }
         }
 
         private class LinkedListTask
         {
+            Node _tail;
+            Node _head;
             private class Node
             {
-                public Node next;
-                public int value;
+                public Node(string data)
+                {
+                    Data = data;
+                }
+
+                public Node Next { get; set; }
+
+                public Node Previous { get; set; }
+                public string Data { get; set; }
             }
 
             public void TaskLoop()
             {
+
+               
                 Console.WriteLine("Task started. Enter exit to quit");
-                Console.WriteLine("How elements you want to input? (3-6)");
+                Console.WriteLine("How many elements do you want to input? (3–6))");
                 var result = Console.ReadLine();
-                if(InputOrExit(result)) return;
+                if(IsExit(result)) return;
                 if (!int.TryParse(result, out int a) || a < 3 || a > 6)
                 {
                     Console.WriteLine("Incorrect input. Min 3 elements, max 6 elements");
                     return;
                 }
 
-                List<Node> nodes = Enumerable.Range(0, a).Select(_ => new Node()).ToList();
-
-                for (int i = 0; i < nodes.Count; i++)
+                for (int i = 0; i < a; i++)
                 {
-                    Console.WriteLine($"Input {i+1} element");
-                    result = Console.ReadLine();
-                    if (InputOrExit(result)) return;
-                    if (int.TryParse(result, out int n))
-                    {
-                        nodes[i].value = n;
-                        if (i + 1 < nodes.Count) nodes[i].next = nodes[i + 1];
-
-                    }
+                    AddNode(Console.ReadLine());
                 }
+                Node current = _head;
 
                 Console.WriteLine("\nList");
 
-                for (int i = 0; i < nodes.Count; i++)
+                while(current != null) 
                 {
-                    Console.WriteLine($"\nvalue {nodes[i].value}");
-                    if (nodes[i].next != null) { Console.WriteLine($"next {nodes[i].next.value}"); }
+                    Console.WriteLine($"Node: {current.Data}");
+                    if (current.Next != null) { Console.WriteLine($" Node Next:  {current.Next.Data}"); }
+                    else { Console.WriteLine($"Node  {current.Data} does not have Next"); }
+                    current = current.Next;
                 }
-
 
                 Console.WriteLine("\nReverse List");
-                nodes.Reverse();
 
-                for (int i = 0; i < nodes.Count; i++)
+                current = _tail;
+                while (current != null)
                 {
-                    Console.WriteLine($"\nvalue {nodes[i].value}");
-                    if (nodes[i].next != null) { Console.WriteLine($"next {nodes[i].next.value}"); }
+                    Console.WriteLine($"Node: {current.Data}");
+                    if (current.Previous != null) { Console.WriteLine($" Node Previous:  {current.Previous.Data}"); }
+                    else { Console.WriteLine($"Node  {current.Data} does not have Previous"); }
+                    current = current.Previous;
                 }
+
+            }
+
+            public void AddNode(string data)
+            {
+                Node node = new Node(data);
+
+                if (_head == null)
+                    _head = node;
+                else
+                {
+                    _tail.Next = node;
+                    node.Previous = _tail;
+                }
+                _tail = node;
             }
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter 1,2 or 3 to check task 1,2 or 3");
-            if (int.TryParse(Console.ReadLine(), out int task))
+            Console.WriteLine("Or enter exit to quit");
+            string input = Console.ReadLine();
+            if(IsExit(input)) return;
+            if (int.TryParse(input, out int task))
             {
                 switch (task)
                 {
